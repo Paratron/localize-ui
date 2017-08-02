f515b84cab9e76726091cb1b97ce25bc13a8d362
Christian Engel <christian.engel@wearekiss.com>
2017-08-02 15:45:35 +0200

    Docs update

---
+++ tests.js
@@ -0,0 +1,34 @@
+/**
+ * Localize UI unit tests
+ * ======================
+ */
+
+
+/**
+ * Defining localizations
+ */
+
+// Can a locale be defined?
+
+// Is the first defined locale auto-selected?
+
+// Can multiple locales be defined?
+
+// Can locales be switched on the fly?
+
+
+/**
+ * Accessing localizations
+ */
+
+// Does the accessor work?
+
+// Does the namespaced accessor work?
+
+
+/**
+ * Defining and using formatters
+ */
+
+// Can formatters be defined per locale?
+
